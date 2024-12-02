4a62d30dd574983666f093393cc74a2d30e00576
110550035 <kono.c@nycu.edu.tw>
2024-12-02 15:28:02 +0800

    feat: extract common methods from pthread

--- pthread/pthread.hpp
+++ pthread/pthread.hpp
@@ -226,25 +226,3 @@ Mat dct_2d(const Mat& image, const int& num_threads_assigned = 4, const string& 
     return rw::dct::dct_matrix;
 }
 }  // namespace dct_pthread
-
-// PSNR Calculation
-double calculate_psnr(const Mat& original, const Mat& reconstructed) {
-    Mat diff;
-    absdiff(original, reconstructed, diff);
-    diff.convertTo(diff, CV_32F);
-    diff = diff.mul(diff);
-    double mse = sum(diff)[0] / (original.total());
-    if (mse == 0) return 100;  // No error
-    double max_pixel = 255.0;
-    return 20.0 * log10(max_pixel / sqrt(mse));
-}
-
-map<string, string> parse_args(int argc, char* argv[]) {
-    map<string, string> args;
-    for (int i = 1; i < argc; i++) {
-        string arg = argv[i];
-        if (arg.find("--")) continue;
-        args[arg.substr(2)] = i + 1 < argc && string(argv[i + 1]).find("-") != 0 ? argv[++i] : "";
-    }
-    return args;
-}
