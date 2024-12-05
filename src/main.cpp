d3b2e7bf8b2480e4535603432e2e0b1d74041e22
110550035 <kono.c@nycu.edu.tw>
2024-12-05 22:01:40 +0800

    feat: support setting experiment args through command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 // run following command in root dir of repo to start experiment:
 // $ make clean; make; srun -c 6 -w hpc097 ./bin/main > data/log.txt
 
+#include <iostream>
+
 #include "experiment.hpp"
+#include "lib/util.hpp"
 
 auto main(int argc, char* argv[]) -> int {
-    ExperimentArgs args;
+    auto command_line_args = util::system::parse_args(argc, argv);
+    if (command_line_args.find("num-threads") == command_line_args.end()) {
+        std::cerr << "Usage: ./bin/main --num-threads <threads>" << endl;
+        exit(1);
+    }
+
+    ExperimentArgs args(command_line_args);
     Experiment     experiment;
 
     // array<bool, 2> all_data{false, true};
